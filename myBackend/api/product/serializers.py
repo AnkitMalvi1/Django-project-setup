f8d47507683f7eb98d746f658a2486760880ac94
AnkitMalvi1 <88956779+AnkitMalvi1@users.noreply.github.com>
2023-03-27 16:21:34 +0530

    Add files via upload

---
+++ myBackend/api/product/serializers.py
@@ -0,0 +1,10 @@
+from .models import Product
+from rest_framework import serializers
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image=serializers.ImageField(max_length=None,allow_empty_file=False,allow_null=False,use_url=True,required=False)
+    class Meta:
+        model=Product
+        fields=("id","name","description","price","stock","category")
+                # "image"
+        fields='__all__'
---
+++ myBackend/myBackend/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def default(request):
+    return HttpResponse("<h3>Hello there this is home page </h3>")
